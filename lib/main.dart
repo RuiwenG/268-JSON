e67637b85a847fd0ad893bd1cbaca8dc55e43bf2
Ruiwen Guan <ruiwenguan@gmail.com>
2025-05-14 16:42:01 -0700

    silly error

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:hw5/pages/background.dart';
 import 'package:hw5/pages/manual.dart'; // For compute
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
